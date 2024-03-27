6477187f6b8a7fa7e77bb4efc26df76d560f9442
soullcake <soullcakezz@gmail.com>
2024-03-27 12:06:26 -0300

    registering payment

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -29,7 +29,26 @@ class InvoiceController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'type' => 'required|max:1',
+            'paid' => 'required|numeric|between:0,1',
+            'payment_date' => 'nullable',
+            'value' => 'required|numeric|between:1,9999.99'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $created = Invoice::create($validator->validated());
+
+        if (!$created) {
+            return response()->json(['message' => 'Erro ao criar a fatura.'], 422);
+        }
+    
+        return response()->json(['message' => 'Fatura criada com sucesso.'], 200);
+
     }
 
     /**
