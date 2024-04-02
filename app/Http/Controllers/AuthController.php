81a755685efb10685e59a409b9a7788437c5477c
soullcake <soullcakezz@gmail.com>
2024-04-02 14:52:38 -0300

    chore

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        if(Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message'=>'Autorizado', 
+                'token' => $request->user()->createToken('invoice')->plainTextToken
+            ]);
+        }
+
+        return response()->json(['message'=>'Nao Autorizado']);
+    }
+}
